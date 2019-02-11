cc1a59ca697395bae343e28f5afb77ee59ecdf4c
daliborgogic <mail@daliborgogic.com>
2019-02-11 18:00:51 +0100

    if not equal exit

--- 200/entrypoint.sh
+++ 200/entrypoint.sh
@@ -4,10 +4,6 @@ set -e
 
 STATUS=$(curl -L --max-time 1 -s -o /dev/null -w '%{http_code}' $URL)
 
-if [ $STATUS -eq 200 ]; then
-  echo $STATUS
-  exit 0
-else
-  echo "wtf $STATUS"
+if [ $STATUS -ne 200 ]; then
   exit 1
 fi
