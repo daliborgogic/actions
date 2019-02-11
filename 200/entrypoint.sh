1ab82a64a05aaeef8c06541491ffb4662aa6b082
daliborgogic <mail@daliborgogic.com>
2019-02-11 17:24:23 +0100

    double quote to prevent globbing and word splitting

--- 200/entrypoint.sh
+++ 200/entrypoint.sh
@@ -2,15 +2,15 @@
 
 set -e
 
-while [ $MAX_TRIES -gt 0 ]
+while [ "$MAX_TRIES" -gt 0 ]
 do
-  STATUS=$(curl -L --max-time 1 -s -o /dev/null -w '%{http_code}' $URL)
-  if [ $STATUS -eq 200 ]; then
+  STATUS=$(curl -L --max-time 1 -s -o /dev/null -w '%{http_code}' "$URL")
+  if [ "$STATUS" -eq 200 ]; then
     exit 0
   else
     MAX_TRIES=$((MAX_TRIES - 1))
   fi
-  sleep $SECONDS_BETWEEN_CHECKS
+  sleep "$SECONDS_BETWEEN_CHECKS"
 done
 
 exit 1
